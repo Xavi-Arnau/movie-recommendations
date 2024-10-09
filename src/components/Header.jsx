151259179815b941f98263adcbe014b42737f2bc
Xavi-Arnau <chavier28@gmail.com>
2024-10-09 17:36:59 +0200

    new icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { names } from "../data";
+import { Popcorn } from "lucide-react";
 
 const Header = () => {
   const name = names[Math.floor(Math.random() * names.length)];
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl text-green-500 py-8 font-bold text-center">
-        Recomanacions {name}
+      <h1 className="text-4xl text-green-500 py-8 font-bold text-center flex">
+        <Popcorn size={35} color="yellow" /> Recomanacions {name}
       </h1>
     </div>
   );
